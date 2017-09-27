b8cdeccf783057ae5ab894f507b998f655bf1d08
Gamal Al-Hetari <gn62192@gmail.com>
2017-09-27 16:41:03 +0000

    gram validations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.default_url_options = { host: 'grammable-gamal-hetari.herokuapp.com'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
